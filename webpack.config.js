018b25dacbf8bb79a19b575766cdda7b391bc4aa
JustinJnkns <69018193+JustinJnkns@users.noreply.github.com>
2020-12-20 22:41:58 -0500

    minimized images, lazy loaded javascript

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,49 @@
 const path = require("path")
 const webpack = require("webpack")
-
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin
 module.exports = {
-    entry: './assets/js/script.js',
+    // shows webpack where to start the bundle of dependancies
+    entry: {
+        app: './assets/js/script.js',
+        events: "./assets/js/events.js",
+        schedule: "./assets/js/schedule.js",
+        tickets: "./assets/js/tickets.js"
+    },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+        // name of each attribute in the entry object will be used in place of [name] in each bundle.js created
+        // output will put written into dist folder
+        filename: "[name].bundle.js",
+        path: __dirname + "/dist",
+    },
+    module: {
+        rules: [{
+            test: /\.jpg$/i,
+            use: [{
+                    // user file-loader,before image-webpack-loader so image webpack loader can optimize emitted files.
+                    loader: 'file-loader',
+                    options: {
+                        name(file) {
+                            return "[path][name].[ext]"
+                        },
+                        publicPath: function(url) {
+                            return url.replace("../", "assets/")
+                        }
+                    }
+                },
+                {
+                    loader: 'image-webpack-loader'
+                }
+            ]
+        }]
     },
     plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery",
         }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static",
+        })
     ],
     mode: 'development'
 }
